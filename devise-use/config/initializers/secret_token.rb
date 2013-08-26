79fdea1b5a7176aa44722aee3c6522bf7ad4ed55
Road Tang <roadtang@gmail.com>
2013-08-26 18:55:56 +0800

    my following on the railscast 209 introduct devise

---
+++ devise-use/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseUse::Application.config.secret_token = '4186defe76b45e29645ce9bcc0401fd0bc4289def9321e83ae34b17f8f0656f1b2a64431cba49928ebc20376e9f0873bba0b65482a0414c839e32b8e9088b5ae'
