d0359fec41ab4063176544d580b6e90019ab6819
Road Tang <roadtang@gmail.com>
2013-09-05 00:52:08 +0800

    add assocation between, and replace the assocation views to select and show name

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   belongs_to :supplier
-  attr_accessible :description, :name, :retail_price, :wholesale_price
+  attr_accessible :description, :name, :retail_price, :wholesale_price, :supplier_id
+  validates_presence_of :description, :name, :retail_price, :wholesale_price
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,3 +1,11 @@
 class Supplier < ActiveRecord::Base
   attr_accessible :active, :contact, :name
+  
+  validates_presence_of   :active, :contact, :name
+
+  has_many :products
+
+  def to_s
+    self.name
+  end
 end
