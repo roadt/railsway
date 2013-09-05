2dfa419d21082ea421d0577767d78c78ab45f1f2
Road Tang <roadtang@gmail.com>
2013-09-05 14:49:20 +0800

    add client side validation,  refine validation conditon of product

--- Gemfile
+++ Gemfile
@@ -37,3 +37,4 @@ gem 'jquery-rails'
 
 # To use debugger
 # gem 'debugger'
+gem 'client_side_validations'
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,10 +1,13 @@
 class Supplier < ActiveRecord::Base
+  # attr accessor
   attr_accessible :active, :contact, :name
   
-  validates_presence_of   :active, :contact, :name
-
+  # association
   has_many :products
-
+  
+  #validations
+  validates_presence_of   :active, :contact, :name
+  
   def to_s
     self.name
   end
