819bd57a0cbcc73efa7c95dfe591c5363534d730
Road Tang <roadtang@gmail.com>
2013-12-26 20:40:41 +0800

    railscast 273 geocoder my version

---
+++ geocoder/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geocoder::Application.config.secret_token = '07669e9c7ed958cb2a9d684da9f74ac547f74f2f6bdfe7f8df3dfb596bb143eea5ed1d79cccb5b5f210d9acdecba61e65e62b14eba23f6ce2e497888ebe8d34c'
